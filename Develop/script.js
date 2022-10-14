d04aa07925ac41d8af85d30cd38d7b9fd80baa49
jlewisit <joelewis.mortgage@gmail.com>
2022-10-14 07:18:32 -0700

    reformat HTML, re-wrote past present future function in Js

--- Develop/script.js
+++ Develop/script.js
@@ -4,7 +4,7 @@ $("#currentDay").text(today);
 
 var currentTime = moment();
 
-displayTime.textContent = currentTime.format("dddd, MMMM Do")
+// displayTime.textContent = currentTime.format("dddd, MMMM Do")
 
 $(".saveBtn").on("click", function() {
     // get row text field class and row id values
@@ -21,13 +21,11 @@ $(".saveBtn").on("click", function() {
 var nine = moment().format('09:00');
 $("#hour1").text(nine);
 
-// Static 10AM in moment.js
-var ten = moment().format('10:00');
-$("#hour2").text(ten);
+// 10AM 
+$("#10-row .description").val(localStorage.getItem("10-row" /* localStorage KEY also the row id */));
 
-// Static 11AM in moment.js
-var eleven = moment().format('11:00');
-$("#hour3").text(eleven);
+// 11AM 
+$("#11-row .description").val(localStorage.getItem("11-row" /* localStorage KEY also the row id */));
 
 // Static 12PM in moment.js
 var twelve = moment().format('12:00');
@@ -53,7 +51,91 @@ $("#hour8").text(four);
 var five = moment().format('17:00');
 $("#hour9").text(five);
 
+// converting time and display
+
+var hour9 = 9;
+var hour10 = 10;
+var hour11 = 11;
+var hour12 = 12;
+var hour13 = 13;
+var hour14 = 14;
+var hour15 = 15;
+var hour16 = 16;
+var hour17 = 17;
+
 var currentHour = moment().format('H');
+
+// 10
+if (currentHour < hour10) {
+    $("#10-text").addClass("future");
+} else if (currentHour > hour10) {
+    $("10-text").addClass("past");
+} else if (currentHour === hour10) {
+    $("#10-text").addClass("present");
+}
+
+// 11
+if (currentHour < hour11) {
+    $("#11-text").addClass("future");
+} else if (currentHour > hour11) {
+    $("11-text").addClass("past");
+} else if (currentHour === hour11) {
+    $("#11-text").addClass("present");
+}
+
+// 12
+if (currentHour < hour12) {
+    $("#12-text").addClass("future");
+} else if (currentHour > hour12) {
+    $("12-text").addClass("past");
+} else if (currentHour === hour12) {
+    $("#12-text").addClass("present");
+}
+
+// 1
+if (currentHour < hour13) {
+    $("#1-text").addClass("future");
+} else if (currentHour > hour13) {
+    $("1-text").addClass("past");
+} else if (currentHour === hour13) {
+    $("#1-text").addClass("present");
+}
+
+// 2
+if (currentHour < hour14) {
+    $("#2-text").addClass("future");
+} else if (currentHour > hour14) {
+    $("#2-text").addClass("past");
+} else if (currentHour === hour14) {
+    $("#2-text").addClass("present");
+}
+
+// 3
+if (currentHour < hour15) {
+    $("#3-text").addClass("future");
+} else if (currentHour > hour15) {
+    $("3-text").addClass("past");
+} else if (currentHour === hour15) {
+    $("#3-text").addClass("present");
+}
+
+// 4
+if (currentHour < hour16) {
+    $("#4-text").addClass("future");
+} else if (currentHour > hour16) {
+    $("4-text").addClass("past");
+} else if (currentHour === hour16) {
+    $("#4-text").addClass("present");
+}
+
+// 5
+if (currentHour < hour17) {
+    $("#5-text").addClass("future");
+} else if (currentHour > hour17) {
+    $("5-text").addClass("past");
+} else if (currentHour === hour17) {
+    $("#5-text").addClass("present");
+}
 
 // check time block for past/present/future hour and add class changing background color
 $(".time-block").each(function () {
@@ -84,25 +166,25 @@ $(".time-block").each(function () {
 //     localStorage.setItem("userNote", JSON.stringify(userNote));
 // }
 
-var inpKey = document.getElementById("inpKey");
-// const inpValue = docuemnt.getElementById("inpValue");
-var btnInsert = document.getElementById("btnInsert");
-var lsOutput = document.getElementById("lsOutput");
-inpKey = "note1Key";
-btnInsert = "save1";
-lsOutput = "note1Output";
-save1.onclick = function() {
-    const note = note1Key.value;
-    // const value = inValue.value;
-console.log(note);
-// console.log(value);
-// if (note) {
-//     localStorage.setItem(note);
-//     location.reload();
-// }
-for (let i = 0; i < localStorage.length; i++) {
-    const note = localStorage.key(i);
-    // const value = localStorage.getItem(note);
-    note1Output.innerHTML += `${note}<br />`;
-    }
- }
+// var inpKey = document.getElementById("inpKey");
+// // const inpValue = docuemnt.getElementById("inpValue");
+// var btnInsert = document.getElementById("btnInsert");
+// var lsOutput = document.getElementById("lsOutput");
+// inpKey = "note1Key";
+// btnInsert = "save1";
+// lsOutput = "note1Output";
+// save1.onclick = function() {
+//     const note = note1Key.value;
+//     // const value = inValue.value;
+// console.log(note);
+// // console.log(value);
+// // if (note) {
+// //     localStorage.setItem(note);
+// //     location.reload();
+// // }
+// for (let i = 0; i < localStorage.length; i++) {
+//     const note = localStorage.key(i);
+//     // const value = localStorage.getItem(note);
+//     note1Output.innerHTML += `${note}<br />`;
+//     }
+//  }
